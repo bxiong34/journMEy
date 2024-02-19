b69d2d33eb0392b4629818f0f4abda7aa5494a59
bxiong34 <111828461+bxiong34@users.noreply.github.com>
2024-02-19 11:50:27 -0600

    installed necessary packages & added tailwind css

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,21 @@
+const typeDefs = `
+  // model type  {
+    _id: ID!
+
+  }
+
+  // model type  {
+    _id: ID!
+
+  }
+
+  type Query {
+
+  }
+
+  type Mutation {
+
+  }
+`;
+
+module.exports = typeDefs;
