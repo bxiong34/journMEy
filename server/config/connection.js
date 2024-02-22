537a9058c65179397a8e2c2a48bd0cb121f2a9b0
Willie Jones <williejones432@yahoo.com>
2024-02-21 19:09:24 -0600

    Added Review Model

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
 // connect to mongodb database
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/journMEy');
 
 module.exports = mongoose.connection;
