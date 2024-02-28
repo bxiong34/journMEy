dc6c71cfde1122d5b2012c3f07390d15a07a9da4
Keng <kengthao57@gmail.com>
2024-02-27 21:47:25 -0600

    Added addToFavorite button and rendered markers onto Account

--- client/src/pages/Account.jsx
+++ client/src/pages/Account.jsx
@@ -1,35 +1,53 @@
 import React, { useState, useEffect } from "react";
-import { useQuery } from "@apollo/client";
-import { QUERY_USER } from "../utils/queries";
 
 function Account() {
-  const { loading, data } = useQuery(QUERY_USER);
-  const [reviews, setReviews] = useState([]);
+  const [favorites, setFavorites] = useState([]);
 
   useEffect(() => {
-    if (!loading && data) {
-      // Assuming data.reviews is an array of user's past reviews
-      setReviews(data.reviews);
+    const storedFavorites = localStorage.getItem("favorites");
+    if (storedFavorites) {
+      setFavorites(JSON.parse(storedFavorites));
     }
-  }, [loading, data]);
+  }, []);
+
+  const removeFromFavorites = (id) => {
+    const updatedFavorites = favorites.filter((marker) => marker.id !== id);
+    setFavorites(updatedFavorites);
+    localStorage.setItem("favorites", JSON.stringify(updatedFavorites));
+  };
+
+  // const locateOnMap = (coordinates) => {
+  //   if (coordinates) {
+  //     mapRef.current.flyTo(coordinates, 13, { animate: true });
+  //     console.log("Locating coordinates on the map:", coordinates);
+  //   } else {
+  //     console.log("Coordinates are undefined");
+  //   }
+  // };
+
 
   return (
     <div className="w-full h-screen bg-[#1a1a1a] text-white flex-row text-center">
-      <h2 className="text-4xl">Hello User!</h2>
-      <p>Add a Review?</p>
-      <p>View Favorites?</p>
-      <p>View Past Reviews?</p>
-      <ul className="pastReviewsList">
-        {reviews.map((review, index) => (
-          <li key={index}>
-            <strong>Review:</strong> {review.review}
-            <br />
-            <strong>Rating:</strong> {review.rating}
-            <br />
-            <strong>Created At:</strong>{" "}
-            {new Date(review.createdAt).toLocaleDateString()}
-          </li>
-        ))}
+      <h2 className="text-4xl">Favorites</h2>
+      <ul className="favoritesList">
+      {favorites.map((marker) => (
+  <li key={marker.id}>
+    <strong>City Name:</strong> {marker.cityName}
+    <br />
+    <strong>Review 1:</strong> {marker.review1}
+    <br />
+    <strong>Review 2:</strong> {marker.review2}
+    <br />
+    <strong>Rating:</strong> {marker.rating}
+    <br />
+    <button onClick={() => removeFromFavorites(marker.id)}>
+      Remove
+    </button>
+    <button onClick={() => locateOnMap(marker.geocode)}>
+    Locate on Map
+    </button>
+  </li>
+))}
       </ul>
     </div>
   );
--- client/src/pages/Map.jsx
+++ client/src/pages/Map.jsx
@@ -1,4 +1,4 @@
-import "../App.css";
+import React, { useState, useEffect, useRef } from "react";
 import L from "leaflet"; // Import Leaflet library
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
@@ -6,16 +6,12 @@ import { Icon, divIcon, point } from "leaflet";
 import MarkerClusterGroup from "react-leaflet-cluster";
 import markers from "../components/markers";
 import useGeoLocation from "../components/useGeoLocation";
-import { useRef } from "react";
 import "leaflet-control-geocoder/dist/Control.Geocoder.css";
 import "leaflet-control-geocoder/dist/Control.Geocoder.js";
 import LeafletControlGeocoder from "../components/LeafletControlGeocoder";
-import "leaflet-control-geocoder";
-
 
 const customIcon = new Icon({
   iconUrl: "https://cdn-icons-png.flaticon.com/128/9131/9131546.png",
-  // size of icon
   iconSize: [38, 38],
 });
 
@@ -26,29 +22,50 @@ const createCustomClusterIcon = (cluster) => {
   });
 };
 
-const addToFavorite = (marker) => {
-  console.log("Marker added to account:", marker);
-};
-
 function Leafletmap() {
   const location = useGeoLocation();
-
+  const [favorites, setFavorites] = useState([]);
   const mapRef = useRef(null);
-
   const zoom = 13;
+
+  useEffect(() => {
+    const storedFavorites = localStorage.getItem("favorites");
+    if (storedFavorites) {
+      setFavorites(JSON.parse(storedFavorites));
+    }
+  }, []);
+
+const addToFavorite = (marker) => {
+  const favoritesData = localStorage.getItem("favorites");
+  const favorites = favoritesData ? JSON.parse(favoritesData) : [];
+  const id = Date.now(); // Generate a unique ID based on current timestamp
+  const updatedFavorites = [...favorites, { id: id, ...marker }];
+  localStorage.setItem("favorites", JSON.stringify(updatedFavorites));
+  setFavorites(updatedFavorites);
+  console.log("Marker added to favorites:", marker);
+};
 
   const showMyLocation = () => {
     if (mapRef.current && location.loaded && !location.error) {
-      // Check if mapRef.current exists
       mapRef.current.flyTo(
         [location.coordinates.lat, location.coordinates.lng],
         zoom,
         { animate: true },
-      ); // Use mapRef.current directly
+      );
     } else if (location.error) {
       alert(location.error.message);
     }
   };
+
+  // const locateOnMap = (coordinates) => {
+  //   if (coordinates) {
+  //     mapRef.current.flyTo(coordinates, zoom, { animate: true });
+  //     console.log("Locating coordinates on the map:", coordinates);
+  //   } else {
+  //     console.log("Coordinates are undefined");
+  //   }
+  // };
+
 
   return (
     <div className="h-screen bg-[#1a1a1a] text-white flex items-center flex-col">
@@ -61,16 +78,15 @@ function Leafletmap() {
         worldCopyJump={true}
       >
         <TileLayer
-          // attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           attribution='<a href="https://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank">&copy; <b>Jawg</b>Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          // url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
           url="https://tile.jawg.io/jawg-lagoon/{z}/{x}/{y}{r}.png?access-token={accessToken}"
           accessToken="9OHyglPUFdqXritzAZGFb1IXCvZgQBGFArRhOQ819CcqZZfWONdETSlMIIMaDoLU"
         />
-         <LeafletControlGeocoder />
+        <LeafletControlGeocoder />
 
         {location.loaded && !location.error && (
           <Marker
+            key="currentLocation"
             icon={customIcon}
             position={[location.coordinates.lat, location.coordinates.lng]}
           ></Marker>
@@ -81,21 +97,34 @@ function Leafletmap() {
           iconCreateFunction={createCustomClusterIcon}
         >
           {markers.map((marker, index) => (
-    <Marker key={index} position={marker.geocode} icon={customIcon}>
-      <Popup style={{ paddingLeft: "20px" }}>
-        <strong>{marker.cityName}</strong>
-        <div style={{ marginLeft: "20px" }}>
-          {marker.review1}
-          <br />
-          <br />
-          {marker.review2}
-          <br />
-          <button onClick={() => addToFavorite(marker)} style={{backgroundColor: "#008CBA", color: "white", padding: "10px 20px", borderRadius: "5px", border: "none", display: "block", margin: "10px auto 0",}}>Add to Account</button> {/* Add button */}
-        </div>
-      </Popup>
-    </Marker>
-  ))}
-</MarkerClusterGroup>
+            <Marker key={`${marker.id}-${index}`} position={marker.geocode} icon={customIcon}>
+              <Popup style={{ paddingLeft: "20px" }}>
+                <strong>{marker.cityName}</strong>
+                <div style={{ marginLeft: "20px" }}>
+                  {marker.review1}
+                  <br />
+                  <br />
+                  {marker.review2}
+                  <br />
+                  <button
+                    onClick={() => addToFavorite(marker)}
+                    style={{
+                      backgroundColor: "#008CBA",
+                      color: "white",
+                      padding: "10px 20px",
+                      borderRadius: "5px",
+                      border: "none",
+                      display: "block",
+                      margin: "10px auto 0",
+                    }}
+                  >
+                    Add to Account
+                  </button>
+                </div>
+              </Popup>
+            </Marker>
+          ))}
+        </MarkerClusterGroup>
       </MapContainer>
 
       <div
