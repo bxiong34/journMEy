2801dc99f13d26aafe35af84cb0ba06de81b8931
bxiong34 <111828461+bxiong34@users.noreply.github.com>
2024-03-01 15:18:05 -0600

    npm i

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 // connect to mongodb database
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/journMEy');
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = mongoose.connection;
