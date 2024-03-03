8bd374ccab60c4f994be39a1db6f68e2877284cf
bxiong34 <111828461+bxiong34@users.noreply.github.com>
2024-03-03 12:26:10 -0600

    added ./ path file in vit.config

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Specify the base URL for your assets
+    base: './',
+  },
     server: {
     port: 3000,
     open: true,
