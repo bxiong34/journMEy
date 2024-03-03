ac2f20a07d9a397ab785aabc14df1c8f6815b867
Willie Jones <williejones432@yahoo.com>
2024-03-03 00:39:21 -0600

    update server

--- server/server.js
+++ server/server.js
@@ -35,11 +35,11 @@ app.use(cors());
 
    // if in production, make client/dist files available for download or view in app
   if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/dist')));
+    app.use(express.static(path.join(__dirname, '../client/dist')));
   }
 
   app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
   });
   } 
 
