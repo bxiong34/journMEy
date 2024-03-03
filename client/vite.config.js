2e5cde1ac3f85f4ba78c546f9ac06c1083a334d7
bxiong34 <111828461+bxiong34@users.noreply.github.com>
2024-03-03 14:00:56 -0600

    npm i

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,11 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    base: './',
-    assetsDir: 'assets',
-    assetsInlineLimit: 0,
-  },
     server: {
     port: 3000,
     open: true,
