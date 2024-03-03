f1975eefe89de63768f7d56c369b83ae76e7dfc5
Willie Jones <williejones432@yahoo.com>
2024-03-02 23:35:56 -0600

    npm updated

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 // connect to mongodb database
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/journMEy");
 
 module.exports = mongoose.connection;
